e824754f1d9e90e30a23d9e725438b051ba2fb0b
Oluwasegun David Olumbe <david.olumbe@visilinx.com>
2017-05-19 23:09:41 +0100

    create signup form add axios

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,14 @@ let app = new Vue({
 	el: '#app',
 	data: {
 		message: 'My name is ',
-		name: ''
+		name: '',
+		picked: ''
 	}
 });
+
+
+axios.get('https://api.github.com/users/' + username)
+	.then(function(response){
+		console.log(response.data); // ex.: { user: 'Your User'}
+		console.log(response.status); // ex.: 200
+	});
